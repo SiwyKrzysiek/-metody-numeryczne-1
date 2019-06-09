d6fb230a811f4d0fbb4670a567815a3c10488ca9
Krzysztof Dabrowski <01133318@pw.edu.pl>
2019-06-09 20:21:09 +0200

    Dopasowanie wykresu do książki

--- Kod/zadanie_z_ksiazki.m
+++ Kod/zadanie_z_ksiazki.m
@@ -9,36 +9,18 @@ t = [0 1 2 3]
 f = [1 3 -2 -1]
 x = transformuj(t)
 
-% T = transformujOdwrotnie(x, 0, 3)
-
-% punkty = linspace(0, 2*pi, 1000);
 a = -1;
 b = 4;
 punkty = linspace(a, b, 1000);
-%punkty = transformuj(punkty);
 punkty = transformujPunkty(punkty, length(t));
 
 y = interpolacjaTygonometryczna(x, f, punkty);
 
-subplot(2, 1, 1);
-title("Bez transformacji odworotnej");
-hold on;
+title("Wykres interpolacji danych z ksi??ki po transformacji odwrotnej");
+xlabel("t")'
+ylabel("f(t)");
 grid on;
-plot(x, f, "o");
-
-plot(punkty, y, "-");
-
-subplot(2, 1, 2);
-title("Po transformacji odwrotnej");
 hold on;
-grid on;
-plot(transformujOdwrotnie(x, 0, 3), f, "o");
+plot(transformujOdwrotnie(x, 0, 3), f, "o", 'DisplayName','W?z?y interpolacji');
 
-% plot(punkty, y, "-");
-plot(transformujOdwrotnie(punkty, a, b), y, "-");
-
-
-
-% e = linspace(-1, 4, 6)
-% E = transformuj(e)
-% F = transformujOdwrotnie(E, -1, 4)
+plot(transformujOdwrotnie(punkty, a, b), y, "-", 'DisplayName','Funkcja interpoluj?ca f');
